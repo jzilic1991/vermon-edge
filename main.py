84d513949ecf7a7a5cfc51d5e3d1ca181a96d800
jzilic1991 <jzilic91@gmail.com>
2023-04-03 07:28:29 -0700

    Integrating flask server and monpoly wrapper

--- main.py
+++ main.py
@@ -2,6 +2,56 @@ import asyncio
 
 from flask import Flask, request, jsonify
 
+
+async def create_mon_instance ():
+
+	subprocess = await asyncio.create_subprocess_exec("monpoly", "-sig", "edge-monitoring/netper.sig", "-formula", \
+	"edge-monitoring/netper.mfotl", stdin = asyncio.subprocess.PIPE, stdout = asyncio.subprocess.PIPE)
+	print ("Subprocess: " + str(subprocess))
+
+	return subprocess
+
+
+# main coroutine
+async def monitor (trace, process):
+	
+	# create as a subprocess using create_subprocess_exec
+	# f = open("edge-monitoring/netper.log", "r")
+	
+	# for l in f.readlines():
+	# 	# write data to the subprocess
+	# 	process.stdin.write(bytes (l, 'utf-8'))
+	# 	await process.stdin.drain()
+		
+	# 	try:
+	# 		line = await asyncio.wait_for(process.stdout.readline(), 0.01)
+		
+	# 		if line:
+	# 			line = line.decode()
+	# 			print (line)
+
+	# 	except:
+	# 		continue
+	
+	process.stdin.write(bytes (trace, 'utf-8'))
+	print ("Monitor process: " + str (process))
+	await process.stdin.drain()
+		
+	try:
+		line = await asyncio.wait_for(process.stdout.readline(), 1)
+		
+		if line:
+			line = line.decode()
+			print ("Line:" + line)
+			return line
+
+	except:
+		
+		return ""
+
+
+
+process = asyncio.run (create_mon_instance ())
 app = Flask (__name__)
 
 @app.route('/edge-vermon')
@@ -9,34 +59,10 @@ def trace_handler ():
 
 	trace = request.args.get('trace', None)
 	print ("Trace: " + trace)
+	print ("Process: " + str (process))
 
-	asyncio.run (monitor (trace))
+	return jsonify ([asyncio.run (monitor (trace, process))])
 
-	return jsonify ([])
-
-
-# main coroutine
-async def monitor (trace):
-	
-	# create as a subprocess using create_subprocess_exec
-	process = await asyncio.create_subprocess_exec("monpoly", "-sig", "edge-monitoring/netper.sig", "-formula", \
-	"edge-monitoring/netper.mfotl", stdin = asyncio.subprocess.PIPE, stdout = asyncio.subprocess.PIPE)
-	f = open("edge-monitoring/netper.log", "r")
-	
-	for l in f.readlines():
-		# write data to the subprocess
-		process.stdin.write(bytes (l, 'utf-8'))
-		await process.stdin.drain()
-		
-		try:
-			line = await asyncio.wait_for(process.stdout.readline(), 0.01)
-		
-			if line:
-				line = line.decode()
-				print (line)
-
-		except:
-			continue
 
 
 if __name__ == "__main__":
