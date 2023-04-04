6c694e312e8969fc4606753311416a3c310b3dee
jzilic1991 <jzilic91@gmail.com>
2023-04-04 09:21:59 -0700

    Integrating Monpoly wrapper process and and async task

--- main.py
+++ main.py
@@ -1,67 +1,65 @@
 import asyncio
 
+from multiprocessing import Process, Queue
 from flask import Flask, request, jsonify
 
 
-async def create_mon_instance ():
+class Monpoly (Process):
 
-	subprocess = await asyncio.create_subprocess_exec("monpoly", "-sig", "edge-monitoring/netper.sig", "-formula", \
-	"edge-monitoring/netper.mfotl", stdin = asyncio.subprocess.PIPE, stdout = asyncio.subprocess.PIPE)
-	print ("Subprocess: " + str(subprocess))
+	def __init__ (self, t_q, v_q):
 
-	return subprocess
-
-
-# main coroutine
-async def monitor (trace, process):
-	
-	# create as a subprocess using create_subprocess_exec
-	# f = open("edge-monitoring/netper.log", "r")
-	
-	# for l in f.readlines():
-	# 	# write data to the subprocess
-	# 	process.stdin.write(bytes (l, 'utf-8'))
-	# 	await process.stdin.drain()
-		
-	# 	try:
-	# 		line = await asyncio.wait_for(process.stdout.readline(), 0.01)
-		
-	# 		if line:
-	# 			line = line.decode()
-	# 			print (line)
-
-	# 	except:
-	# 		continue
-	
-	process.stdin.write(bytes (trace, 'utf-8'))
-	print ("Monitor process: " + str (process))
-	await process.stdin.drain()
-		
-	try:
-		line = await asyncio.wait_for(process.stdout.readline(), 1)
-		
-		if line:
-			line = line.decode()
-			print ("Line:" + line)
-			return line
-
-	except:
-		
-		return ""
+		Process.__init__(self)
+		self._t_q = t_q
+		self._v_q = v_q
 
 
+	def run (cls):
 
-process = asyncio.run (create_mon_instance ())
+		asyncio.run (cls.__processing ())
+
+
+	async def __processing (cls):
+
+		proc = await asyncio.create_subprocess_exec("monpoly", "-sig", "edge-monitoring/netper.sig", "-formula", \
+		 	"edge-monitoring/netper.mfotl", stdin = asyncio.subprocess.PIPE, stdout = asyncio.subprocess.PIPE)
+
+		while True:
+
+			trace = cls._t_q.get ()
+			
+			if trace:
+
+				proc.stdin.write(bytes (trace, 'utf-8'))
+				await proc.stdin.drain()
+
+				try:
+					line = await asyncio.wait_for (proc.stdout.readline(), 0.1)
+					cls._v_q.put (line.decode ())
+
+				except Exception:
+				
+					cls._v_q.put ("")
+					
+
 app = Flask (__name__)
+t_q = Queue ()
+v_q = Queue ()
+mon = Monpoly (t_q, v_q)
+mon.start ()
 
 @app.route('/edge-vermon')
 def trace_handler ():
 
+	global t_q, v_q
+	
 	trace = request.args.get('trace', None)
 	print ("Trace: " + trace)
-	print ("Process: " + str (process))
 
-	return jsonify ([asyncio.run (monitor (trace, process))])
+	t_q.put (trace)
+	v = v_q.get ()
+	print ("Verdict: " + str(v))
+
+	return jsonify ([v])
 
 
 
